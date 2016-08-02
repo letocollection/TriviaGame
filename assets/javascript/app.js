6cac0d8666f967bf21a24041a6f4c2edec56ac3c
letocollection <leonard@letocollection.com>
2016-08-02 15:21:46 -0700

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//Trivia Game Pseudo Code//
+
+//1. Create an 9 questions. Each questions will have 4 possible multiple choice answers. 
+//2. All 9 questions will be formated as Objects. Each 4 answer object will be an array. There needs to be a function that marks 1 answer to be the right answer.  
+//3. Only the begginning of the game will have a start button. There will be no start button after that ever. Game will initialize with start button. 
+//4. Question will display at random. Answers for the questions will also be in random order. Each answer will have a mouse-enter, mouse-leave function. 
+//5. If answer is right, right answer counter will go up 1. Will have a gif display of right answer. Will use giphy API. 
+//6. If answer is wrong, wrong answer counter will go up by 1. Will have right answer gif still display regardless. 
+//7. Whether answer is right or wrong, the next question will immediately show up. 
